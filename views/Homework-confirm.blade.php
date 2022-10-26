53f6b69ab253f0f6b5d750b77ae6af4209d12993
shin-f0318 <sf.0318ss@gmail.com>
2022-10-26 20:38:43 +0900

    laravel2

---
+++ views/Homework-confirm.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="課題返答">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>課題返答</title>
+    </head>
+
+    <body>
+        <h2>結果</h2>
+        <p><?php echo $post['number']; ?></p>
+        
+    </body>
+</html>
---
+++ views/Homework.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="課題">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>課題</title>
+    </head>
+    <body>
+        <h2>点数(1~100)を入力してください！！</h2>
+        <form action="Homework-confirm" method="post">
+        @csrf
+            <div>
+                <input type = "text" name = "number">
+                <input type = "submit" name = "submit" value = "送信">
+            </div> 
+        </form>         
+    </body>
+
+</html>
+    
